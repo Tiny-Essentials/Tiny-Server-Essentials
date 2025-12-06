226a4cb967f7b635259d6988f1506ef2691f4b26
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2025-12-06 18:50:56 -0300

    socket proxy (beta) added.

---
+++ src/proxy/index.mjs
@@ -0,0 +1,6 @@
+import SocketIoProxyClient from './client/index.mjs';
+import proxyOnConnection from './client/proxyOnConnection.mjs';
+import SocketIoProxyUser from './client/proxyUser.mjs';
+import SocketIoProxyServer from './server/index.mjs';
+
+export { SocketIoProxyServer, SocketIoProxyClient, SocketIoProxyUser, proxyOnConnection };
